0a33afb0c9e6e9192e543f65d09c361e3ffad5e9
Rasit Evduzen <r.evduzen@hotmail.com>
2024-03-25 22:06:33 +0300

    Update!

--- Inverted_Pendulum/main.m
+++ Inverted_Pendulum/main.m
@@ -4,43 +4,67 @@ clc,clear all,close all;
 % Rasit EVDÜZEN
 
 % System Parameter
-m = 1;   % pendulum mass
+m = 5;   % pendulum mass
 M = 5;   % cart mass
 L = 2;   % pendulum length
-g = -10; % gravitational force
-d = 1;   % cart damping
-b = 1;   % pendulum up (b=1) there are 2 fixed here, [0 - down] [pi - up]
+g = -9.81; % gravitational force
+d = .5;   % cart damping
+b = 1;   % pendulum up (b=1) 
 
-% Linear State Space Model 
+% Linear State Space Model
 A = [0  1          0               0;
-     0 -d/M        b*m*g/M         0;
-     0  0          0               1;
-     0 -b*d/(M*L) -b*(m+M)*g/(M*L) 0];
+    0 -d/M        b*m*g/M         0;
+    0  0          0               1;
+    0 -b*d/(M*L) -b*(m+M)*g/(M*L) 0];
 B = [0; 1/M; 0; b*1/(M*L)];
 
-%%  Design LQR controller
+%  Design LQR controller
 Q = eye(length(A));
 R = 0.0001;
 K = lqr(A,B,Q,R);
 
 
-%% Simulate closed-loop system
-tspan = 0:.001:10;
-x0 = [-2; 0; pi; 0];  % initial condition 
-wr = [2; 0; pi; 0];       % reference position
-u  = @(x)-K*(x - wr);     % control law
-[t,x] = ode45(@(t,x)pendcart(x,m,M,L,g,d,u(x)),tspan,x0);  % Runge Kutta4 Based Simulation
+% Simulate closed-loop system
+tspan = 0:2e-3:16;
+x0 = [-6; 0; pi; 0];  % initial condition
+Ref = [6; 0; pi; 0];       % reference position
+u  = @(x)-K*(x - Ref);     % control law
+[t,state] = ode45(@(t,x) pendss(x,m,M,L,g,d,u(x)),tspan,x0);  % Runge Kutta4 for Forward Dynamics Simulation
 
 
-%% PLOT 
-figure,set(gcf,'Position',[100 100 1000 800]) 
-subplot(2,2,[1,2])
-for k=1:100:length(t)
-    drawpend(x(k,:),m,M,L);
+%% PLOT Simulation
+figure('units','normalized','outerposition',[0 0 1 1],'color','w')
+for k=1:1e2:length(t)
+    x = state(k,1);
+    th = state(k,3);
+    % dimensions
+    W  = 1.5*sqrt(M/5);  % cart width
+    H  = .5*sqrt(M/5);   % cart height
+    wr = .5;             % wheel radius
+    mr = .3*sqrt(m);     % mass radius
+    % positions
+    y = wr/2+H/2; % cart vertical position
+    pendx = x + L*sin(th);
+    pendy = y - L*cos(th);
+    % Plot Pendulum
+    clf
+    subplot(2,2,[1,2])
+    plot([-10 10],[0 0],'k--','LineWidth',2), hold on
+    rectangle('Position',[x-W/2,y-H/2,W,H],'Curvature',.1,'FaceColor',[0.4940 0.1840 0.5560],'LineWidth',1.5); % Draw cart
+    rectangle('Position',[x-.9*W/2,0,wr,wr],'Curvature',1,'FaceColor',[1 1 0],'LineWidth',1.5);    % Draw wheel
+    rectangle('Position',[x+.9*W/2-wr,0,wr,wr],'Curvature',1,'FaceColor',[1 1 0],'LineWidth',1.5); % Draw wheel
+    plot([x pendx],[y pendy],'k','LineWidth',2); % Draw pendulum
+    rectangle('Position',[pendx-mr/2,pendy-mr/2,mr,mr],'Curvature',1,'FaceColor',[1 0.1 .1],'LineWidth',1.5);
+    axis([-5 5 -1 3]);, axis equal,grid on
+    title('Inverted Pendulum LQR Based Control')
+    xline(x0(1),'b'),xline(Ref(1),'b')
+
+    hold on
+    subplot(2,2,[3,4])
+    plot(t,state,'LineWidth',2); hold on
+    xline(t(k),'r')
+    legend('x','v','\theta','\omega','time','Location','SouthEast');
+    xlabel('Time'),ylabel('State'),grid on
+    drawnow
 end
-hold on
-subplot(2,2,[3,4])
-plot(t,x,'LineWidth',2); hold on
-l1 = legend('x','v','\theta','\omega');
-set(l1,'Location','SouthEast')
-xlabel('Time'),ylabel('State'),grid on
+
---
+++ PolePlacementVsLQR/PPvsLQR.m
@@ -0,0 +1,53 @@
+%% Pole Placement Design via Ackerman VS LQR
+% Written By: Rasit Evduzen
+% 20-May-2020
+clc,clear all,close all;
+% System Matrix
+A = [0 1 0; 0 0 1; -1 -5 -6];
+B = [0 0 1]';
+J = [-2+4*j -2-4*j -10];  % Close Loop Pole Location
+K = acker(A,B,J);         % Full State Feedback Gain
+
+Q = eye(3);
+R = 1;
+Kopt = lqr(A,B,Q,R);     % Full State Feedback Optimal Gain
+
+sys1 = ss((A-B*K),eye(3),eye(3),eye(3));     % Ackerman State Space Solution
+sys2 = ss((A-B*Kopt),eye(3),eye(3),eye(3));  % Optimal State Space Solution
+
+t = 0:5e-2:15;   % Simulation Time
+% Ackerman
+xa = initial(sys1,[1 2 3]',t);   % System initial State
+% LQR
+xb = initial(sys2,[1 2 3]',t);   % System initial State
+
+
+%% Plot
+figure('units','normalized','outerposition',[0 0 1 1],'color','w')
+
+subplot(3,2,1)
+plot(t(1:i),xa(1:i,1),'k','LineWidth',2),grid,hold on
+plot(t(1:i),xb(1:i,1),'r','LineWidth',2)
+ylabel('State X1'),xlabel('Time'),legend('Ackerman','LQR')
+axis([min(t) max(t) -1 3])
+
+subplot(3,2,3)
+plot(t(1:i),xa(1:i,2),'k','LineWidth',2),grid,hold on
+plot(t(1:i),xb(1:i,2),'r','LineWidth',2)
+ylabel('State X2'),xlabel('Time'),legend('Ackerman','LQR')
+axis([min(t) max(t) -3 3])
+
+
+subplot(3,2,5)
+plot(t(1:i),xa(1:i,3),'k','LineWidth',2),grid,hold on
+plot(t(1:i),xb(1:i,3),'r','LineWidth',2)
+ylabel('State X3'),xlabel('Time'),legend('Ackerman','LQR')
+axis([min(t) max(t) -18 10])
+
+
+subplot(3,2,[2,4,6])
+plot3(xa(1:i,1),xa(1:i,2),xa(1:i,3),'k','LineWidth',2),grid,hold on
+plot3(xb(1:i,1),xb(1:i,2),xb(1:i,3),'r','LineWidth',2)
+title('System Phase Space'),legend('Ackerman','LQR')
+xlabel('State X1'),ylabel('State X2'),zlabel('State X3')
+axis([-1 3 -3 3 -20 10])
